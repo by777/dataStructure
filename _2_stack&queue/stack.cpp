0bd1782997fa8167ec44158d02a0484c1fba6489
by777 <1373953675@qq.com>
2018-06-01 00:50:23 +0800

    链式栈

--- _2_stack&queue/stack.cpp
+++ _2_stack&queue/stack.cpp
@@ -2,12 +2,14 @@
  * @Author: XU BAI <by777>
  * @Date:   29/05/18
  * @Last modified by:   by777
- * @Last modified time: 29/05/18
+ * @Last modified time: 31/05/18
  * @Copyright: All Rights Reserved.
  */
 #include <iostream>
 #include <stdlib.h>
 #define maxSize 100
+#define STACKINCREMENT 10
+#define STACKSIZE 100
 using namespace std;
 
 typedef struct sqStack{
@@ -15,20 +17,24 @@ typedef struct sqStack{
   int top;
 }sqStack;
 
-typedef struct likStack{
-  int* bottom;
-  int* top;
-  int* stacksize;
-}likStack;
+typedef struct stackNode{
+  int data;
+  struct stackNode* next;
+
+}stackNode;
 
 
 int chooseOptions();
+
 sqStack initSqStack();
 int sqPush(sqStack* sqstack, int a[], int length);
-int sqPop(sqStack* sqstack);
+int sqPop(sqStack* sqstack, int &elem);
+
+stackNode* initstackNode();
+int likPush(stackNode* top, int data);
 
 int chooseOptions(){
-  cout<<"plz choose likStack(1) or sqStack(2)"<<endl;
+  cout<<"plz choose stackNode(1) or sqStack(2)"<<endl;
   int option;
   cin>>option;
   switch (option) {
@@ -57,23 +63,47 @@ int sqPush(sqStack* sqstack, int a[], int length){
   return 0;
 }
 
-int sqPop(sqStack* sqstack){
+int sqPop(sqStack* sqstack, int &elem){
 
   if (sqstack->top == -1) {
     cout<<"stack empty"<<endl;
-    return -32767;
+    return sqstack->top;
   }
+  elem = sqstack->elem[sqstack->top--];
+  return sqstack->top;
+}
 
-  return sqstack->elem[sqstack->top--];
+stackNode* initstackNode(){
+  stackNode* top;
+  top = (stackNode*)malloc(sizeof(stackNode));
+  top -> next = NULL;
+  return (top);
+
+}
+
+int likPush(stackNode* top, int data){
+  stackNode* p;
+  p = (stackNode*)malloc(sizeof(stackNode));
+  if (!p) {
+    return -1;
+  }
+  p -> data = data;
+  //尾插
+  p -> next = top -> next;
+  top -> next = p;
+  return 0;
+
 }
 
 int main() {
   sqStack sqstack =  initSqStack();
   int a[10] = {0,1,2,3,4,5,6,7,8,9};
-  int length = 10;
+  int length = 11;
   sqPush(&sqstack,a,length);
+  int elem;
   while (sqstack.top!=-1) {
-    cout<<sqPop(&sqstack);
+    //flag > 0 ,elem接收pop
+    cout<<sqPop(&sqstack,elem);
   }
 
 
