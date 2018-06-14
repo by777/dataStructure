89f72d02a85aa7005bcbed5e98ca9bc69ef5f469
by777 <1373953675@qq.com>
2018-06-15 01:07:54 +0800

    邻接矩阵和邻接表的建立
    待增加图示

--- _5_graph/graph.cpp
+++ _5_graph/graph.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <stdlib.h>
+#include <stdio.h>
 #define INFINITY 65535
-// 最大顶点数，应由用户定义
+// 最大顶点数
 #define MAXVEX 100
 using namespace std;
 
@@ -11,7 +12,51 @@ typedef struct{ //顶点char类型 边int
   int numNodes,numEdges;  //顶点数和边数
 }MGraph;  //图的邻接矩阵
 
-void CreateMGraph(MGraph *G);
+typedef struct EdgeNode{
+  //边表节点
+  int adjvex;//邻节点域，存储该顶点对应的下标
+  int info;//权值
+  struct EdgeNode *next;//指向下一个邻接点
+}EdgeNode;
+
+typedef struct VertexNode{
+  //顶点表节点
+  char data;//顶点域，存储顶点信息
+  EdgeNode *firstedge;//边表头指针
+}VertexNode,AdjList[MAXVEX];
+
+typedef struct{
+  AdjList adjList;
+  int numNodes,numEdges;//图中当前顶点数和边数
+}GraphAdjList;
+
+void CreateMGraph(MGraph *G);//邻接矩阵
+void CreateALGraph(GraphAdjList *G);//邻接表建立
+void CreateALGraph(GraphAdjList *G){
+  int i,k,j;
+  EdgeNode *e;
+  cout<<"Please input numNodes and numEdges "<<endl;
+  scanf("%d,%d",&G->numNodes,&G->numEdges);
+  /* 读入顶点信息,建立顶点表 */
+  for (i = 0; i <G -> numNodes; i++) {
+    scanf(&G->adjList[i].data);//顶点信息
+    G -> adjList[i].firstedge = NULL;//边表置为空表
+  }
+  for(k = 0; k < G -> numEdges; k++){
+    cout << "Input edge(vi vj) numNodes " <<endl;
+    scanf("%d,%d",&i,&j); /* 输入边(vi,vj)上的顶点序号 */
+    e = (EdgeNode *)malloc(sizeof(EdgeNode));//申请边表节点
+    e->adjvex=j;					/* 邻接序号为j */
+    e->next=G->adjList[i].firstedge;	/* 将e的指针指向当前顶点上指向的结点 */
+    G->adjList[i].firstedge=e;		/* 将当前顶点的指针指向e */
+
+    e=(EdgeNode *)malloc(sizeof(EdgeNode)); /* 向内存申请空间,生成边表结点 */
+    e->adjvex=i;					/* 邻接序号为i */
+    e->next=G->adjList[j].firstedge;	/* 将e的指针指向当前顶点上指向的结点 */
+    G->adjList[j].firstedge=e;		/* 将当前顶点的指针指向e */
+  }
+}
+
 void CreateMGraph(MGraph *G){
   int i,j,k,w;
   cout<<"Please input num of vertexs"<<endl;
@@ -26,15 +71,18 @@ void CreateMGraph(MGraph *G){
 		for(j = 0;j <G->numNodes;j++)
 			G->arc[i][j]=INFINITY;	// 邻接矩阵初始化
   for (k = 0 ; k < G -> numEdges; k++) {
-    cout<<"Please inut two subscript (i & j) and one weigh (w)"<<endl;
+    cout<<"Please input two subscript (i & j) and one weigh (w)"<<endl;
     cin>> i >> j >> w;
     G -> arc[i][j] = w;
     G -> arc[j][i] = w;//无向图，矩阵对称
   }
 }
-
+///////////////////增加图结构示意图//////////////////
 int main(){
-  MGraph G;
-  CreateMGraph(&G);
+  // MGraph G;
+  // CreateMGraph(&G);
+  GraphAdjList Q;
+  CreateALGraph(&Q);
+
   return 0;
 }
