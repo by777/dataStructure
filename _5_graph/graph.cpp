1b4369b31999ef03ac08871e91f415b6cdbb1c48
by777 <1373953675@qq.com>
2018-06-21 00:32:43 +0800

    邻接矩阵BFS
    null

--- _5_graph/graph.cpp
+++ _5_graph/graph.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #define INFINITY 65535
+#define maxSize 100
 // 最大顶点数
 #define MAXVEX 100
 using namespace std;
@@ -34,8 +35,50 @@ void CreateMGraph(MGraph *G);//邻接矩阵
 void CreateALGraph(GraphAdjList *G);//邻接表建立
 void MGDFS(MGraph G,int i);
 void MGDFSTraverse(MGraph G);//邻接矩阵深度优先遍历
+void MGBFSTraverse(MGraph *G);//邻接矩阵广度优先遍历
 void CreateMGraphDemo(MGraph *G);//快速创建邻接矩阵Demo
 bool visited[MAXVEX];//邻接矩阵访问标志的数组
+
+void MGBFSTraverse(MGraph *G){
+  /* 图的广度优先搜索遍历（BFS）类似于树的层次遍历。
+   * 它的基本思想是：
+   * 首先访问起始节点v,然后选取与v邻接的全部顶点w1,w2,...进行访问，
+   * 再依次访问与w1，w2...邻接的全部顶点（已经访问过的除外），以此类推。
+   * 简单概括：
+   * 1）任取图中一个节点访问，入队，并将这个节点标记为已访问过。
+   * 2）当队列不空时循环执行：出队，依次检查出队顶点的所有邻接顶点，访问没有被访问过的邻接顶点并将其入队
+   * 3）当队列为空时跳出循环，广度优先搜索完成
+  */
+  int que[maxSize],front = 0,rear=0,i,j;
+  for(i = 0; i<G -> numNodes;i++){
+    visited[i] = 0;
+  }
+  for (i = 0;i<G->numNodes;i++) {
+    //对每一个顶点做循环
+    if (!visited[i]){
+      cout<<"访问节点:"<<G->vexs[i]<<endl;
+      visited[i] = 1;
+      //将此顶点入队列
+      rear = (rear + 1) % maxSize;
+      que[rear] = i;
+      while (front != rear) {
+        front = (front + 1) % maxSize;
+        j = que[front];
+        //判断其它顶点若与当前顶点存在边且未访问过
+        for(j = 0; j < G -> numNodes;j++){
+          if(G -> arc[i][j] == 1 && !visited[j]){
+            cout<<"更换节点:"<<G->vexs[j]<<endl;
+            visited[j] = 1;
+            rear = (rear + 1) % maxSize;
+            que[rear] = j;
+          }
+        }
+      }
+    }
+  }
+}
+
+
 void CreateMGraphDemo(MGraph *G){
   int i,j;
   G -> numEdges = 15;
@@ -81,6 +124,12 @@ void CreateMGraphDemo(MGraph *G){
 
 }
 void MGDFS(MGraph G,int i){
+  /*
+  * 图的深度优先搜索遍历（DFS）类似于二叉树的先序遍历。
+  * 它的基本思想是：
+  * 首先访问出发点v,并将其标记为已访问过，然后选取与v邻接的未被访问的任一个顶点，并访问它
+  * 再选取与w邻接的未被访问过的任一节点并访问，以此重复进行。
+  */
   int j;
   visited[i] = 1;
   printf("%c ", G.vexs[i] );//操作
@@ -157,6 +206,7 @@ int main(){
   MGraph G;
   //CreateALGraph(&Q);
   CreateMGraphDemo(&G);
-  MGDFSTraverse(G);
+  //MGDFSTraverse(G);
+  MGBFSTraverse(&G);
   return 0;
 }
