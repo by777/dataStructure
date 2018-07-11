046175d2f35a5080383f706b113c28311901beab
by777 <1373953675@qq.com>
2018-07-11 23:49:58 +0800

    快速排序

--- _6_sort/simpleSort.cpp
+++ _6_sort/simpleSort.cpp
@@ -5,6 +5,35 @@ using namespace std;
 #define maxSize 100
 
 void InsertSort(int R[],int n);
+void QuickSort(int R[],int low,int high);
+void QuickSort(int R[],int low,int high){
+        //cout<<"/递归"<<"";
+        int temp;
+        int i = low,j = high;
+        if (low<high) {
+                temp=R[low];
+                while (i<j) {
+                        //下面这个循环完成了一趟排序，即数组中小于temp的关键字放在左边，
+                        while (j>i&&R[j]>=temp) {
+                                --j;//从右往左扫描，找到一个小于temp的关键字
+                        }
+                        if(i<j) {
+                                R[i] = R[j];
+                                ++i;
+                        }
+                        while (i<j&&R[i]<temp) {
+                                ++i;
+                        }
+                        if (i<j) {
+                                R[j]=R[i];
+                                --j;
+                        }
+                }
+                R[i] = temp;//将temp放在最终位置
+                QuickSort(R,low,i-1);
+                QuickSort(R,i+1,high);
+        }
+}
 void PrintArr(int R[],int n);
 void PrintArr(int R[],int n){
         cout<<"开始输出数组"<<endl;
@@ -32,7 +61,8 @@ int main() {
         /* code */
         int R[6] = {1,3,14,12,2,3};
         int n = 6;
-        InsertSort(R,n);
+        //InsertSort(R,n);
+        QuickSort(R,0,5);
         PrintArr(R,n);
 
         return 0;
