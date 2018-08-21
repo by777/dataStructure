76d85ea27b48fdf1f5ba8df0a9e0118a941822d2
by777 <1373953675@qq.com>
2018-08-21 23:08:02 +0800

    0

--- _7_search/Search.cpp
+++ _7_search/Search.cpp
@@ -31,8 +31,7 @@ int Sequential_Search2(int *a,int n,int key)
         int i;
         a[0]=key;
         i=n;
-        while(a[i]!=key)
-        {
+        while(a[i]!=key){
                 i--;
         }
         return i;
