faee86e1de5bc670a24c02b40a0cb47ec75b5285
by777 <1373953675@qq.com>
2018-08-26 23:15:17 +0800

    5

--- _7_search/Search.cpp
+++ _7_search/Search.cpp
@@ -8,7 +8,7 @@
 #define ERROR 0
 #define TRUE 1
 #define FALSE 0
-#define MAXSIZE 100 /* 存储空间初始分配量 */
+#define MAXSIZE 100 /* 存储空     	间初始分配量 */
 
 typedef int Status; /* Status是函数的类型,其值是函数结果状态代码，如OK等 */
 
