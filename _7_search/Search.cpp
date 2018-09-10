847980e98927446b99a1cd3e4ecae9c98d35677f
by777 <1373953675@qq.com>
2018-09-10 23:19:54 +0800

    11

--- _7_search/Search.cpp
+++ _7_search/Search.cpp
@@ -43,8 +43,7 @@ int Binary_Search(int *a,int n,int key)
         int low,high,mid;
         low=1; /* 定义最低下标为记录首位 */
         high=n; /* 定义最高下标为记录末位 */
-        while(low<=high)
-        {
+        while(low<=high)        {
                 mid=(low+high)/2; /* 折半 */
                 if (key<a[mid]) /* 若查找值比中值小 */
                         high=mid-1; /* 最高下标调整到中位下标小一位 */
